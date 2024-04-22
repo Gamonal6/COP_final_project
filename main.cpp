f9ab336c7c6b6f47d9785b551f3adc005b3919d3
Gamonal <henriquegamonal3@gmail.com>
2024-04-22 14:50:47 -0400

    changed input logic and added insert_funcion to decrease inout request redundance.

--- main.cpp
+++ main.cpp
@@ -49,6 +49,24 @@ bool TaskNode::Compare::operator()(const TaskNode *n1,
                   : n1->priority > n2->priority;
 }
 
+void insert_task(HeapQueue<TaskNode *, TaskNode::Compare>& pqf) {
+  int date[2];
+  int estimated_completion_time;
+  char delimiter = '/';
+  string task;
+  cout << "Insert a task you would like to work on: " << endl;
+  getline(cin, task);
+  cout << "When is it due(MM/DD)? " << endl;
+  cin >> date[0]>> delimiter >>date[1]; 
+  cout << "How many minutes will it take? (Enter a whole number): " << endl;
+  cin >> estimated_completion_time;
+
+  std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+  TaskNode *node = new TaskNode(priority, minutes, task, description);
+  pqf.insert(node);
+  
+}
+
 int main() {
   
   HeapQueue<TaskNode *, TaskNode::Compare> pq;
@@ -61,10 +79,13 @@ int main() {
   // add the task node to the priority queue pq
   // remove the number from the list of integers
   // repeat until the list of integers is empty
-
+  bool new_task = true;
+  bool terminate_program = false;
+  
+  
   int n;
-  cout << "Enter the number of tasks you would like to do today:" << endl;
-  cin >> n;
+  
+
   std::cin.ignore(numeric_limits<streamsize>::max(), '\n'); //mixing cin and getline is disastrous, this is needed to clear the /n and let getline read sth
 
   for (int i = 0; i < n; i++){
@@ -90,47 +111,65 @@ int main() {
     pq.insert(node);
   }
   
-while (!pq.empty()) {
-  cout << "Your next priority is: ";
-  cout << pq.min()->task << endl;
-  cout << "done with a task? (Enter y for yes and n for no) ";
-  string status;
-  cin >> status;
-  string i;
-  if (status == "y"){
-    cout << "What task did you complete? (enter the number of the task): ";
-    cin >> i;
-    int x = stoi(i);
-    pq.removeAtIndex(x);
-    HeapQueue<TaskNode *, TaskNode::Compare> toPrint = pq;
-    while (!toPrint.empty()){
-      cout << toPrint.min()->task << endl;
-      toPrint.removeMin();
-    }
+while ((!pq.empty() || new_task == true) && terminate_program == false) {
+  if (new_task == true){
+    insert_task(pq);
+    
   }
-  else
-  {
+  else{
     cout << "Your next priority is: ";
     cout << pq.min()->task << endl;
-    cout << "Did you start working on your most important task? (Enter y for yes and n for no) " << endl;
-    string start;
-    int MinutesWorked;
+    cout << "When you are done with the task, yes to complete the task or no if you wanna stop working on it? (Enter y for yes and n for no) ";
+    string status;
+    cin >> status;
     string i;
-    cin >> start;
-    if (start == "n"){
-      cout << "You need to " << pq.min()->task << " for " << pq.min()->minutes;
+    if (status == "y"){
+      pq.removeMin();
+      HeapQueue<TaskNode *, TaskNode::Compare> toPrint = pq;
+      while (!toPrint.empty()){
+        cout << toPrint.min()->task << endl;
+        toPrint.removeMin();
+      }
     }
     else
     {
-      cout << "What task did you start? (enter the number of the task): ";
-      cin >> i;
-      int x = stoi(i);
-      cout << "How many minutes did you work on the task? " << endl;
-      cin >> MinutesWorked;
-      //pq.PeekAtIndex(x);
+      if (pq.empty()){
+        cout << "You are done with all your tasks! Would you like to insert a new task or are you done for the day? (y for new task, n for done)" << endl;
+
+        cin >> status;
+
+        if (status == "n"){
+          terminate_program = true;
+          break;
+        }
+        else {
+          
+        }
+      }
+
+      cout << "Your next priority is: ";
+      cout << pq.min()->task << endl;
+      cout << "Did you start working on your most important task? (Enter y for yes and n for no) " << endl;
+      string start;
+      int MinutesWorked;
+      string i;
+      cin >> start;
+      if (start == "n"){
+        cout << "You need to " << pq.min()->task << " for " << pq.min()->minutes;
+      }
+      else
+      {
+        cout << "What task did you start? (enter the number of the task): ";
+        cin >> i;
+        int x = stoi(i);
+        cout << "How many minutes did you work on the task? " << endl;
+        cin >> MinutesWorked;
+        //pq.PeekAtIndex(x);
+      }
+      
     }
-    
-  }
+    }
+  
   
 
 }
