f602660390b348ad10d03b4af5444bece25db395
Abdul Moeed Zahid <abdulmoeedzahid@usf.edu>
2024-04-21 22:38:26 -0400

    Here are some of the changes I have made. Added a peek function to heapqueue, cleaned up the UI, and tried implementing a subtract minutes function for when the user says "n"

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <limits>
 #include "heapqueue.hpp"
 
 using namespace std;
@@ -6,10 +7,12 @@ using namespace std;
 class TaskNode {
 public:
     int priority;
+    int minutes;
     string task;
+    string description;
 
     // Constructor
-    TaskNode(int p, string t) : priority(p), task(t) {}
+    TaskNode(int p, int m, string t, string d) : priority(p), minutes(m), task(t) , description(d){}
 
 class Compare {
 public:
@@ -20,9 +23,19 @@ private:
   bool lessThan;
 };
 
+class SubtractMinutes {
+public:
+  int operator()(TaskNode *node, int minutes);
+};
     // Destructor (if needed)
     // ~Node() {}
 };
+
+int TaskNode::SubtractMinutes::operator()(TaskNode *node, int minutes) {
+  node->minutes -= minutes;
+  return node->minutes;
+}
+
 
 bool TaskNode::Compare::operator()(const TaskNode &n1,
                                       const TaskNode &n2) const {
@@ -31,7 +44,7 @@ bool TaskNode::Compare::operator()(const TaskNode &n1,
 }
 
 bool TaskNode::Compare::operator()(const TaskNode *n1,
-                                      const TaskNode *n2) const {
+                                      const TaskNode *n2) const { //What if tasks have equal priority
   return lessThan ? n1->priority < n2->priority
                   : n1->priority > n2->priority;
 }
@@ -41,37 +54,51 @@ int main() {
     HeapQueue<TaskNode *, TaskNode::Compare> pq;
 
   // CREATING A TODO LIST:
-    // prompt the use for number of tasks n
-   // create a list of integers from 1 to the n
+  // prompt the use for number of tasks n
+  // create a list of integers from 1 to the n
   // ask the user for the task
-   // ask the user for the priority of the task ranging from 1 to n from 1 being the highest and n being the lowest
+  // ask the user for the priority of the task ranging from 1 to 10 from 1 being the highest and 10 being the lowest
   // add the task node to the priority queue pq
   // remove the number from the list of integers
   // repeat until the list of integers is empty
 
   int n;
-  cout << "Enter the number of tasks you woudl like to do today? ";
+  cout << "Enter the number of tasks you would like to do today:" << endl;
   cin >> n;
+  std::cin.ignore(numeric_limits<streamsize>::max(), '\n'); //mixing cin and getline is disastrous, this is needed to clear the /n and let getline read sth
+
   for (int i = 0; i < n; i++){
     int priority;
-    string task = "";
-    cout << "Enter the task: ";
-    cin >> task;
-    cout << "what is the priority of this task? ";
+    int minutes;
+    string task;
+    string description;
+
+    cout << "Enter the task: " << endl;
+    getline(cin, task);
+
+    cout << "Describe the task: " << endl;
+    getline(cin,description);
+
+    cout << "On a scale from 1-10 with 1 being the highest, What is the priority of this task: (Enter a whole number)" << endl;
     cin >> priority;
 
-    TaskNode *node = new TaskNode(priority, task);
+    cout << "How many minutes will it take? (Enter a whole number): " << endl;
+    cin >> minutes;
+
+    std::cin.ignore(numeric_limits<streamsize>::max(), '\n');
+    TaskNode *node = new TaskNode(priority, minutes, task, description);
     pq.insert(node);
   }
   
+while (!pq.empty()) {
   cout << "Your next priority is: ";
   cout << pq.min()->task << endl;
-  cout << "done with a task? ";
-  string yes;
-  cin >> yes;
+  cout << "done with a task? (Enter y for yes and n for no) ";
+  string status;
+  cin >> status;
   string i;
-  if (yes == "y"){
-    cout << "What task did you complete? ";
+  if (status == "y"){
+    cout << "What task did you complete? (enter the number of the task): ";
     cin >> i;
     int x = stoi(i);
     pq.removeAtIndex(x);
@@ -81,6 +108,32 @@ int main() {
       toPrint.removeMin();
     }
   }
+  else
+  {
+    cout << "Your next priority is: ";
+    cout << pq.min()->task << endl;
+    cout << "Did you start working on your most important task? (Enter y for yes and n for no) " << endl;
+    string start;
+    int MinutesWorked;
+    string i;
+    cin >> start;
+    if (start == "n"){
+      cout << "You need to " << pq.min()->task << " for " << pq.min()->minutes;
+    }
+    else
+    {
+      cout << "What task did you start? (enter the number of the task): ";
+      cin >> i;
+      int x = stoi(i);
+      cout << "How many minutes did you work on the task? " << endl;
+      cin >> MinutesWorked;
+      pq.PeekAtIndex(x);
+      HeapQueue<TaskNode *, TaskNode::SubtractMinutes> toPrint = pq;
+    }
+    
+  }
   
+
+}
   
 }
